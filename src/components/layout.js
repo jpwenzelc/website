344990ffb8dcf5be660b14c204149489d90a543b
devwenzel <jose.wenzel@codurance.com>
2020-02-18 17:10:18 +0000

    add toggle button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,61 +1,114 @@
 import React from "react"
 import { Link } from "gatsby"
+import Toggle from "../components/toggle"
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
 
 import { rhythm, scale } from "../utils/typography"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
+class Layout extends React.Component {
 
-  header = (
-    <h1
-      style={{
-        ...scale(1.5),
-        marginBottom: rhythm(1.5),
-        marginTop: 0,
-      }}
-    >
-      <Link
+  state = {
+    theme: null,
+  }
+
+  componentDidMount() {
+    this.setState({ theme: window.__theme })
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme })
+    }
+  }
+
+  renderHeader() {
+    let { location, title } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+
+    return (
+      <h1
         style={{
-          boxShadow: `none`,
-          textDecoration: `none`,
-          color: `inherit`,
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
         }}
-        to={`/`}
       >
-        {title}
-        <span style={{
-          color: '#fe6337'
-        }}>_</span>
-      </Link>
-    </h1>
-  )
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+          <span style={{
+            color: "#fe6337",
+          }}>_</span>
+        </Link>
+      </h1>
+    )
+  }
 
-  return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer style={{
-        ...scale(-.5),
-        textAlign: `center`,
-      }}>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org" target={`_blank`}>Gatsby.</a>
-        {` `}
-        using the
-        {` `}
-        <a href="https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/" target={`_blank`}>Starter Blog</a>
-      </footer>
-    </div>
-  )
+  render() {
+    let { children } = this.props
+
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        {this.renderHeader()}
+        {this.state.theme !== null ? (
+          <Toggle
+            icons={{
+              checked: (
+                <img
+                  src={faMoon}
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: "none" }}
+                />
+              ),
+              unchecked: (
+                <img
+                  src={faSun}
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: "none" }}
+                />
+              ),
+            }}
+            checked={this.state.theme === "dark"}
+            onChange={e =>
+              window.__setPreferredTheme(
+                e.target.checked ? "dark" : "light",
+              )
+            }
+          />
+        ) : (
+          <div style={{ height: "24px" }}/>
+        )}
+        <main>{children}</main>
+        <footer style={{
+          ...scale(-.5),
+          textAlign: `center`,
+        }}>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org" target={`_blank`}>Gatsby.</a>
+          {` `}
+          using the
+          {` `}
+          <a href="https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/" target={`_blank`}>Starter Blog</a>
+        </footer>
+      </div>
+    )
+
+  }
 }
 
 export default Layout
